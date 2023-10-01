90cc867934ced428f08f64049bf2df5997be0d19
Razvan Deaconescu <razvan.deaconescu@upb.ro>
2023-10-02 00:05:07 +0300

    src: Align teminating backslash
    Align terminating backslash in `DIE()` macro.
    
    Signed-off-by: Razvan Deaconescu <razvan.deaconescu@upb.ro>

--- src/helpers.h
+++ src/helpers.h
@@ -12,12 +12,12 @@
 #include <unistd.h>
 
 #define DIE(assertion, call_description)						\
-	do {														\
-		if (assertion) {										\
-			fprintf(stderr, "(%s, %d): ", __FILE__, __LINE__);	\
-			perror(call_description);							\
-			exit(errno);										\
-		}														\
+	do {										\
+		if (assertion) {							\
+			fprintf(stderr, "(%s, %d): ", __FILE__, __LINE__);		\
+			perror(call_description);					\
+			exit(errno);							\
+		}									\
 	} while (0)
 
 /* Structure to hold memory block metadata */
